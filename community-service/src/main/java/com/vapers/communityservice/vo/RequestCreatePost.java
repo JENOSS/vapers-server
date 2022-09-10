bcc3ed55a0aa377afef146a74a643f88883683b8
PARK JIN WOO <nari1539@gmail.com>
2022-09-10 14:53:02 +0900

    community-service 제거

--- community-service/src/main/java/com/vapers/communityservice/vo/RequestCreatePost.java
+++
@@ -1,8 +0,0 @@
-package com.vapers.communityservice.vo;
-
-public class RequestCreatePost {
-    private String userToken;
-    private String title;
-    private String contents;
-    private Integer category;
-}
